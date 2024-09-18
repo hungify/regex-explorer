dde101908ef41d43545615a0df8b6422d6e64047
hungify <nmhungify@gmail.com>
2024-09-18 22:32:43 +0700

    feat: Add WorkspaceHeader component with Tabs functionality

---
+++ app/components/WorkspaceHeader.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+const tabs = ['Documents', 'Both', 'Canvas']
+const activeTab = ref('Documents')
+</script>
+
+<template>
+  <div class="flex w-full justify-center items-center gap-2">
+    <Tabs default-value="account">
+      <TabsList v-model="activeTab">
+        <TabsTrigger
+          v-for="tab in tabs" :key="tab"
+          :value="tab"
+        >
+          {{ tab }}
+        </TabsTrigger>
+      </TabsList>
+      <!-- <TabsContent value="account">
+        Make changes to your account here.
+      </TabsContent>
+      <TabsContent value="password">
+        Change your password here.
+      </TabsContent> -->
+    </Tabs>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { appName } from '~/constants'
 import InputExpression from '~/components/InputExpression.vue'
+import WorkspaceHeader from '~/components/WorkspaceHeader.vue'
 
 useHead({
   title: appName,
@@ -11,6 +12,9 @@ useHead({
   <div>
     <div class="relative w-full p-2 flex items-center justify-center gap-2 border-b border-blue-200/80 dark:border-blue-300/10 border-dashed bg-gradient-to-r from-transparent via-blue-100/40 to-transparent  dark:from-transparent dark:via-blue-600/10 dark:to-transparent text-sm text-accent-blue dark:text-blue-100 transition-colors fv-style">
       <InputExpression />
+    </div>
+    <div class="relative w-full p-2 flex items-center justify-center gap-2 border-b border-blue-200/80 dark:border-blue-300/10 border-dashed bg-gradient-to-r from-transparent via-blue-100/40 to-transparent  dark:from-transparent dark:via-blue-600/10 dark:to-transparent text-sm text-accent-blue dark:text-blue-100 transition-colors fv-style">
+      <WorkspaceHeader />
     </div>
   </div>
 </template>
