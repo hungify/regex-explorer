b3c67fdd8bc09e8a33a4b93766333d572ee3e301
hungify <nmhungify@gmail.com>
2024-09-18 22:32:03 +0700

    refactor: Replace RhombusMarker component with RhombusDecorator component in InputExpression.vue

--- app/components/InputExpression.vue
+++ app/components/InputExpression.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { Copy, Flag } from 'lucide-vue-next'
-import { Input } from './ui/input'
+import { Flag, Link, Sparkle } from 'lucide-vue-next'
 import FlagsExpression from './FlagsExpression.vue'
-import RhombusMarker from './RhombusMarker.vue'
+import RhombusDecorator from './RhombusDecorator.vue'
 import { Button } from './ui/button'
+import { Input } from './ui/input'
 import { Separator } from './ui/separator'
+import type { ExpressionFlags, ExpressionTokens } from '~/types/expression'
 import { EXPRESSION_METADATA } from '~/constants/expression'
 import {
   Tooltip,
@@ -12,12 +13,27 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from '@/components/ui/tooltip'
-import type { ExpressionFlags, ExpressionTokens } from '~/types/expression'
 
 const expressionFlags = ref([])
 
 const hoveredToken = ref<ExpressionTokens>('/-open')
 const hoveredFlag = ref<ExpressionFlags>('empty')
+const regexPattern = ref('')
+
+function handlePasteExpression(event: ClipboardEvent) {
+  event.preventDefault()
+  const plainText = event.clipboardData?.getData('text/plain')
+  if (plainText) {
+    if (regexPattern.value.length === 0) {
+      // remove '/' in start and end of the expression
+      const strippedExpression = plainText.replace(/^\/|\/$/g, '')
+      regexPattern.value = strippedExpression
+    }
+    else {
+      regexPattern.value += plainText
+    }
+  }
+}
 
 function handleHoverToken(key: ExpressionTokens) {
   hoveredToken.value = key
@@ -30,104 +46,136 @@ function handleHoverFlag(flag: ExpressionFlags) {
 
 <template>
   <div class="flex w-full max-w-3xl items-center gap-2">
-    <div class="flex items-center relative flex-grow w-full">
-      <TooltipProvider :delay-duration="100">
-        <Tooltip>
-          <TooltipTrigger as="div">
-            <span
-              class="hover:text-green-400 absolute start-0 inset-y-0 flex items-center justify-center px-2 cursor-pointer"
-              @mouseover.prevent="handleHoverToken('/-open')"
-            >
-              /
-            </span>
-          </TooltipTrigger>
-          <TooltipContent :side-offset="24">
-            <strong>{{ EXPRESSION_METADATA.tokens[hoveredToken].title }}</strong>
-            <p>{{ EXPRESSION_METADATA.tokens[hoveredToken].desc }}</p>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-
-      <Input id="regex" placeholder="Insert your expression here" class="pl-8 pr-28 w-full focus-visible:ring-[0.1px] focus-visible:ring-green-600 focus-visible:ring-offset-2" />
-
-      <div class="absolute end-0 inset-y-0 flex items-center justify-center gap-2">
+    <RhombusDecorator
+      :position="{
+        topLeft: true,
+        bottomLeft: true,
+        topRight: true,
+        bottomRight: true,
+      }"
+    >
+      <div class="flex items-center relative flex-grow w-full">
         <TooltipProvider :delay-duration="100">
           <Tooltip>
             <TooltipTrigger as="div">
               <span
-                class="hover:text-green-400 cursor-pointer px-2"
-                @mouseover.prevent="handleHoverToken('/-close')"
+                class="hover:text-green-400 absolute start-0 inset-y-0 flex items-center justify-center px-2 cursor-pointer"
+                @mouseover.prevent="handleHoverToken('/-open')"
               >
                 /
               </span>
             </TooltipTrigger>
-            <TooltipContent :side-offset="14">
+            <TooltipContent :side-offset="24">
               <strong>{{ EXPRESSION_METADATA.tokens[hoveredToken].title }}</strong>
               <p>{{ EXPRESSION_METADATA.tokens[hoveredToken].desc }}</p>
             </TooltipContent>
           </Tooltip>
         </TooltipProvider>
 
-        <Separator orientation="vertical" class="bg-green-400 h-2/3" />
+        <Input
+          id="regex"
+          v-model="regexPattern"
+          placeholder="Insert your expression here"
+          class="pl-8 pr-36 w-full focus-visible:ring-[0.1px] focus-visible:ring-green-600 focus-visible:ring-offset-2"
+          @paste="handlePasteExpression"
+        />
 
-        <FlagsExpression v-model="expressionFlags">
-          <Button
-            variant="ghost"
-            size="xs"
-            class="p-0 min-w-8 h-full hover:bg-blue-700/10 space-x-1.5"
-          >
-            <TooltipProvider :delay-duration="100">
-              <Tooltip>
-                <TooltipTrigger as="div">
-                  <template v-if="expressionFlags.length > 0 ">
+        <div class="absolute end-0 inset-y-0 flex items-center justify-center gap-2">
+          <TooltipProvider :delay-duration="100">
+            <Tooltip>
+              <TooltipTrigger as="div">
+                <span
+                  class="hover:text-green-400 cursor-pointer px-2"
+                  @mouseover.prevent="handleHoverToken('/-close')"
+                >
+                  /
+                </span>
+              </TooltipTrigger>
+              <TooltipContent :side-offset="14">
+                <strong>{{ EXPRESSION_METADATA.tokens[hoveredToken].title }}</strong>
+                <p>{{ EXPRESSION_METADATA.tokens[hoveredToken].desc }}</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <Separator orientation="vertical" class="bg-green-400 h-2/3" />
+
+          <FlagsExpression v-model="expressionFlags">
+            <Button
+              variant="ghost"
+              size="xs"
+              class="p-0 min-w-8 h-full hover:bg-blue-700/10 space-x-1.5"
+            >
+              <TooltipProvider :delay-duration="100">
+                <Tooltip>
+                  <TooltipTrigger as="div">
+                    <template v-if="expressionFlags.length > 0 ">
+                      <Button
+                        v-for="flag in expressionFlags"
+                        :key="flag"
+                        variant="link"
+                        size="xs"
+                        class="hover:text-green-400 p-1"
+                        @mouseover.prevent="handleHoverFlag(flag)"
+                      >
+                        {{ flag }}
+                      </Button>
+                    </template>
                     <Button
-                      v-for="flag in expressionFlags"
-                      :key="flag"
+                      v-else
                       variant="link"
-                      size="xs"
-                      class="hover:text-green-400 p-1"
-                      @mouseover.prevent="handleHoverFlag(flag)"
+                      size="icon"
+                      class="hover:text-green-400"
+                      @mouseover.prevent="handleHoverFlag('empty')"
                     >
-                      {{ flag }}
+                      <Flag class="w-4 h-4" />
                     </Button>
-                  </template>
-                  <Button
-                    v-else
-                    variant="link"
-                    size="icon"
-                    class="hover:text-green-400"
-                    @mouseover.prevent="handleHoverFlag('empty')"
-                  >
-                    <Flag class="w-4 h-4" />
-                  </Button>
-                </TooltipTrigger>
-                <TooltipContent>
+                  </TooltipTrigger>
                   <TooltipContent>
-                    <strong>{{ EXPRESSION_METADATA.flags[hoveredFlag].title }}</strong>
-                    <p>{{ EXPRESSION_METADATA.flags[hoveredFlag].desc }}</p>
+                    <TooltipContent>
+                      <strong>{{ EXPRESSION_METADATA.flags[hoveredFlag].title }}</strong>
+                      <p>{{ EXPRESSION_METADATA.flags[hoveredFlag].desc }}</p>
+                    </TooltipContent>
                   </TooltipContent>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          </Button>
-        </FlagsExpression>
+                </Tooltip>
+              </TooltipProvider>
+            </Button>
+          </FlagsExpression>
+
+          <Separator orientation="vertical" class="bg-green-400 h-2/3" />
+
+          <TooltipProvider :delay-duration="100">
+            <Tooltip>
+              <TooltipTrigger as="div">
+                <Button
+                  variant="link"
+                  size="icon"
+                  class="hover:text-green-400"
+                  @mouseover.prevent="handleHoverFlag('empty')"
+                >
+                  <Link class="w-4 h-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent :side-offset="14">
+                <p>Share your expression</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
       </div>
-    </div>
 
-    <TooltipProvider :delay-duration="100">
-      <Tooltip>
-        <TooltipTrigger>
-          <Button variant="outline" size="icon" class="hover:bg-blue-700/10 hover:text-green-400">
-            <Copy class="w-4 h-4" />
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent>
-          <p>Copy to clipboard</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-
-    <RhombusMarker position="left" />
-    <RhombusMarker position="right" />
+      <TooltipProvider :delay-duration="100">
+        <Tooltip>
+          <TooltipTrigger>
+            <Button variant="outline" size="icon" class="hover:bg-blue-700/10 hover:text-green-400">
+              <Sparkle class="w-4 h-4" />
+            </Button>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Visualize your expression</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </RhombusDecorator>
   </div>
 </template>
--- app/components/TheHeader.vue
+++ app/components/TheHeader.vue
@@ -1,13 +1,10 @@
 <script lang="ts" setup>
 import TheLogo from './TheLogo.vue'
-import RhombusMarker from './RhombusMarker.vue'
 </script>
 
 <template>
   <header class="sticky top-0 z-20 w-full max-w-full flex items-center border-b border-dashed border-blue-200 dark:border-blue-300/20">
     <div class="w-full relative mx-3 md:mx-12 flex border-l border-r border-dashed border-blue-200 dark:border-blue-300/20">
-      <RhombusMarker position="left" />
-      <RhombusMarker position="right" />
       <nav aria-label="Site's main navigation" data-orientation="horizontal" dir="ltr" class="relative z-[2] md:max-w-[1248px] md:mx-auto w-full py-3 px-4 md:px-6 flex justify-between gap-4 lg:gap-0">
         <div class="flex gap-3 items-center flex-none">
           <NuxtLink class="rounded hover:scale-105 transition-transform fv-style" href="/">
