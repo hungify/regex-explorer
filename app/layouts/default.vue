8b0bd8a96c9da72b6b3214913c12148a2ae148f5
hungify <nmhungify@gmail.com>
2024-09-29 13:50:54 +0700

    style: remove unused

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@ import TheHeader from '~/components/TheHeader.vue'
 </script>
 
 <template>
-  <div class="relative flex min-h-screen w-screen flex-col flex-none justify-between">
+  <div class="flex flex-col h-full">
     <TheHeader />
     <main class="mx-3 md:mx-12 grow border-r border-l border-dashed border-blue-200 dark:border-blue-300/20">
       <slot />
