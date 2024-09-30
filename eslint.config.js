e3fe20062514543d58e268ed22a91e511f50819c
hungify <nmhungify@gmail.com>
2024-09-30 22:19:39 +0700

    chore: Add eslint-plugin-tailwindcss

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 import antfu from '@antfu/eslint-config'
+import eslintPluginTailwindCSS from 'eslint-plugin-tailwindcss'
 import nuxt from './.nuxt/eslint.config.mjs'
+
+const tailwind = eslintPluginTailwindCSS.configs['flat/recommended']
 
 export default nuxt(
   antfu(
     {
-      formatters: true
+      formatters: true,
     },
+    tailwind,
   ),
 )
