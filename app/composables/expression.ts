3fb483ec7006e4d3fa328ac801b1979e0dcc5d62
hungify <nmhungify@gmail.com>
2024-09-30 22:20:45 +0700

    refactor: Update useExpression composable to include additional functionality

--- app/composables/expression.ts
+++ app/composables/expression.ts
@@ -1,14 +1,29 @@
 import {
+  RegExpParser,
   RegExpValidator,
 } from '@eslint-community/regexpp'
+import { DEFAULT_PATTERN, DEFAULT_TEST_STRING } from '~/constants/expression'
+
+const validator = new RegExpValidator()
 
 export function useExpression() {
-  const patternRegex = useState<string>('expession', () => '([A-Z])\\w+')
+  const flags = useState<string[]>('flags', () => ['g'])
+  const pattern = useState<string>('expession', () => DEFAULT_PATTERN)
+  const source = useState<string>('source', () => DEFAULT_TEST_STRING)
 
-  const isValidPattern = computed(() => {
-    const validator = new RegExpValidator()
+  const hasGlobalFlag = computed(() => flags.value.includes('g'))
+
+  const expressionRegExp = computed(() => {
+    return new RegExp(pattern.value, flags.value.join(''))
+  })
+
+  const expressionString = computed(() => {
+    return `/${pattern.value}/${flags.value.join('')}`
+  })
+
+  const isValidExpression = computed(() => {
     try {
-      validator.validatePattern(patternRegex.value)
+      validator.validateLiteral(expressionString.value)
       return true
     }
     // eslint-disable-next-line unused-imports/no-unused-vars
@@ -17,8 +32,46 @@ export function useExpression() {
     }
   })
 
+  const isValidPattern = computed(() => {
+    try {
+      validator.validatePattern(pattern.value)
+      return true
+    }
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    catch (e) {
+      return false
+    }
+  })
+
+  const matchers = computed(() => {
+    if (!isValidExpression.value)
+      return []
+
+    if (hasGlobalFlag.value) {
+      return Array.from(source.value.matchAll(expressionRegExp.value))
+    }
+    return source.value.match(expressionRegExp.value)
+  })
+
+  const ast = computed(() => {
+    if (!isValidExpression.value) {
+      return null
+    }
+
+    const parser = new RegExpParser()
+    return parser.parsePattern(pattern.value)
+  })
+
   return {
-    patternRegex,
+    pattern,
+    source,
     isValidPattern,
+    isValidExpression,
+    ast,
+    flags,
+    matchers,
+    expressionString,
+    hasGlobalFlag,
+    expressionRegExp,
   }
 }
