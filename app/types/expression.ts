e82688fcded66adf5cae78c092a481d2b5400688
hungify <nmhungify@gmail.com>
2024-10-01 22:29:58 +0700

    feat: add some types for quick refercen

--- app/types/expression.ts
+++ app/types/expression.ts
@@ -2,3 +2,20 @@ export type ExpressionFlags = 'g' | 'i' | 'm' | 's' | 'u' | 'empty'
 export type ExpressionTokenBasic = '/-open' | '/-close' | '^' | '$' | '.' | '|' | '\\'
 
 export type ExpressionTokens = ExpressionTokenBasic
+
+export type PatternKeys = 'allTokens' | 'commonTokens' | 'generalTokens' | 'anchors' | 'metaSequences' | 'quantifiers' | 'groupConstructs' | 'characterClasses' |
+  'flagsModifiers' | 'substitution'
+
+interface TokenEntry {
+  label: string
+  token: string
+  explanation: string
+}
+
+export interface PatternEntry {
+  key: string
+  label: string
+  icon: Component
+  items: Array<TokenEntry>
+}
+export type PatternReferences = Record<PatternKeys, PatternEntry>
