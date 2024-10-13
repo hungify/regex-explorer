7543fae27563333274b80329fa1a2f7c6914ef41
hungify <nmhungify@gmail.com>
2024-10-13 22:28:32 +0700

    refactor: update CommandDialog and CommandInput components

--- app/components/ui/command/CommandDialog.vue
+++ app/components/ui/command/CommandDialog.vue
@@ -3,15 +3,22 @@ import { useForwardPropsEmits } from 'radix-vue'
 import type { DialogRootEmits, DialogRootProps } from 'radix-vue'
 import Command from './Command.vue'
 
-const props = defineProps<DialogRootProps>()
+interface Props extends DialogRootProps {
+  contentClass?: string
+}
+
+const { contentClass, ...restProps } = defineProps<Props>()
 const emits = defineEmits<DialogRootEmits>()
 
-const forwarded = useForwardPropsEmits(props, emits)
+const forwarded = useForwardPropsEmits(restProps, emits)
 </script>
 
 <template>
   <UiDialog v-bind="forwarded">
-    <UiDialogContent class="overflow-hidden p-0 shadow-lg">
+    <UiDialogContent
+      class="overflow-hidden p-0 shadow-lg"
+      :class="contentClass"
+    >
       <Command class="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:size-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:size-5">
         <slot />
       </Command>
